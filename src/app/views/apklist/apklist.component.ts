8aad0525971299a8abcaaf8b0bdd36a6e5e852db
Ezequiel <85073571+Ezequielp19@users.noreply.github.com>
2024-09-03 18:50:40 -0300

    Update apklist.component.ts

--- src/app/views/apklist/apklist.component.ts
+++ src/app/views/apklist/apklist.component.ts
@@ -39,6 +39,7 @@ export class ApkListComponent implements OnInit {
   constructor(private firestoreService: FirestoreService,private router: Router, private file: File,  // Inyecta File
     private http: HttpClient, // Inyecta HttpClient
     private platform: Platform  // Inyecta Platform
+private fileOpener: FileOpener
 ) {}
 
   ngOnInit() {
@@ -60,19 +61,40 @@ export class ApkListComponent implements OnInit {
     this.categoryFilter$.next(this.selectedCategory);
   }
 
-async downloadApk(url: string) {
-  try {
+// async downloadApk(url: string) {
+//   try {
 
-    await Browser.open({ url });
-  } catch (error) {
-    console.error('Error al abrir el navegador:', error);
+//     await Browser.open({ url });
+//   } catch (error) {
+//     console.error('Error al abrir el navegador:', error);
+//   }
+// }
+
+//   isAndroidTV(): boolean {
+//     return this.platform.is('android') && (navigator.userAgent.includes('TV') || navigator.userAgent.includes('AFT'));
+//   }
+
+
+ async downloadAndInstallApk(url: string) {
+    try {
+      const fileName = url.split('/').pop()!; // Extrae el nombre del archivo del URL
+      const filePath = this.file.dataDirectory + fileName; // Define la ruta donde se guardará el APK
+
+      // Descarga el APK usando HttpClient
+      const response = await this.http.get(url, { responseType: 'blob' }).toPromise();
+      const blob = new Blob([response], { type: 'application/vnd.android.package-archive' });
+
+      // Guarda el archivo APK
+      this.file.writeFile(this.file.dataDirectory, fileName, blob, { replace: true }).then(() => {
+        // Abre el archivo APK para la instalación
+        this.fileOpener.open(filePath, 'application/vnd.android.package-archive')
+          .then(() => console.log('APK abierto para instalación'))
+          .catch((error) => console.error('Error al abrir el APK:', error));
+      }).catch(error => console.error('Error al guardar el archivo APK:', error));
+    } catch (error) {
+      console.error('Error al descargar o instalar el APK:', error);
+    }
   }
-}
-
-  isAndroidTV(): boolean {
-    return this.platform.is('android') && (navigator.userAgent.includes('TV') || navigator.userAgent.includes('AFT'));
-  }
-
 
 
  
