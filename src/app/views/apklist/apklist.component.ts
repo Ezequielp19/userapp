e487a3cd560b0bd0252f6ed96338bab2ba9f4998
Ezequiel <ezegonzalez800@gmail.com>
2024-09-03 19:11:54 -0300

    ion button

--- src/app/views/apklist/apklist.component.ts
+++ src/app/views/apklist/apklist.component.ts
@@ -1,4 +1,4 @@
- import { IonicModule,Platform } from '@ionic/angular';
+import { IonicModule, Platform } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FirestoreService } from '../../common/services/firestore.service';
@@ -6,44 +6,32 @@ import { Apk } from '../../common/models/apk.model';
 import { Categoria } from './../../common/models/categoria.models';
 import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { FormsModule } from '@angular/forms';  // Asegúrate de importar FormsModule
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Browser } from '@capacitor/browser';
-
-
-import { File } from '@awesome-cordova-plugins/file/ngx'; // Importa File
-import { HttpClient, HttpClientModule } from '@angular/common/http'; // Importa HttpClient
-
-import { Filesystem, Directory, Encoding, FilesystemEncoding } from '@capacitor/filesystem';
-import { FileOpener } from '@awesome-cordova-plugins/file-opener2/ngx';
-
-
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-apk-list',
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule,HttpClientModule],
+  imports: [IonicModule, CommonModule, FormsModule],
   templateUrl: './apklist.component.html',
   styleUrls: ['./apklist.component.scss'],
-providers: [FileOpener], 
-
 })
 export class ApkListComponent implements OnInit {
   apks$: Observable<Apk[]>;
-
-   categorias$: Observable<Categoria[]>;
+  categorias$: Observable<Categoria[]>;
   filteredApks$: Observable<Apk[]>;
   selectedCategory: string = '';
+  private categoryFilter$ = new BehaviorSubject<string>('');
 
-    private categoryFilter$ = new BehaviorSubject<string>('');
-
-
-  constructor(private firestoreService: FirestoreService,private router: Router, private file: File,  // Inyecta File
-    private http: HttpClient, // Inyecta HttpClient
-    private platform: Platform  // Inyecta Platform
-private fileOpener: FileOpener
-) {}
+  constructor(
+    private firestoreService: FirestoreService,
+    private router: Router,
+    private http: HttpClient,
+    private platform: Platform
+  ) {}
 
   ngOnInit() {
     this.apks$ = this.firestoreService.getApks();
@@ -64,43 +52,26 @@ private fileOpener: FileOpener
     this.categoryFilter$.next(this.selectedCategory);
   }
 
-// async downloadApk(url: string) {
-//   try {
-
-//     await Browser.open({ url });
-//   } catch (error) {
-//     console.error('Error al abrir el navegador:', error);
-//   }
-// }
-
-//   isAndroidTV(): boolean {
-//     return this.platform.is('android') && (navigator.userAgent.includes('TV') || navigator.userAgent.includes('AFT'));
-//   }
-
-
- async downloadAndInstallApk(url: string) {
+  async downloadAndInstallApk(url: string) {
     try {
-      const fileName = url.split('/').pop()!; // Extrae el nombre del archivo del URL
-      const filePath = this.file.dataDirectory + fileName; // Define la ruta donde se guardará el APK
+      const fileName = url.split('/').pop()!;
+      const filePath = `${Filesystem.getUri({ directory: Directory.Data, path: fileName })}`;
 
       // Descarga el APK usando HttpClient
       const response = await this.http.get(url, { responseType: 'blob' }).toPromise();
       const blob = new Blob([response], { type: 'application/vnd.android.package-archive' });
 
       // Guarda el archivo APK
-      this.file.writeFile(this.file.dataDirectory, fileName, blob, { replace: true }).then(() => {
-        // Abre el archivo APK para la instalación
-        this.fileOpener.open(filePath, 'application/vnd.android.package-archive')
-          .then(() => console.log('APK abierto para instalación'))
-          .catch((error) => console.error('Error al abrir el APK:', error));
-      }).catch(error => console.error('Error al guardar el archivo APK:', error));
+      await Filesystem.writeFile({
+        path: fileName,
+        data: blob,
+        directory: Directory.Data
+      });
+
+      // Abre el archivo APK
+      await Browser.open({ url: filePath });
     } catch (error) {
-      console.error('Error al descargar o instalar el APK:', error);
+      console.error('Error al descargar o abrir el APK:', error);
     }
   }
-
-
- 
-
-
 }
